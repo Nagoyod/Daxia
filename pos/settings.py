1e01f22766779c7c00290264a65f306958fffb26
Nagoyod <armandoyogan77@gmail.com>
2025-06-19 22:54:54 +0800

    Update settings.py

--- pos/settings.py
+++ pos/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posApp.apps.posAppConfig',
     'django.contrib.humanize',
+  
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [ 
+   'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,12 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+BASE_DIR / "static",
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    # location of your application, should not be public web accessible 
     './static',
 )
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
