f0ef78be3a1223b612378f808c9f9bcd6e1b3ccf
Nagoyod <armandoyogan77@gmail.com>
2025-06-19 18:37:46 +0800

    Update settings.py

--- pos/settings.py
+++ pos/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'pos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
-        'USER': 'your_db_user',
-        'PASSWORD': 'your_db_password',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
